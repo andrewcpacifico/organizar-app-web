de270136a295424835c9a33eaea3017102760e7d
Andrew C. Pacifico <andrewcpacifico@gmail.com>
2017-04-17 21:28:29 -0400

    test(Task): fix tests for Task class

--- src/app/task.spec.ts
+++ src/app/task.spec.ts
@@ -2,6 +2,7 @@ import {Task} from './task';
 
 describe('Task', () => {
   it('should create an instance', () => {
-    expect(new Task()).toBeTruthy();
+    const t = new Task(0, 'A task', 'A task desc', 'task-icon');
+    expect(t).toBeTruthy();
   });
 });
